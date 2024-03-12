e1317cd2b8ffb72ac78499109d5ca5c1bc332db9
Douglas Sumita <douglas.sumita@gmail.com>
2024-03-12 15:54:16 +0000

    Desenvolvido rota para registrar novo usuário, configurado para redirecionar após cadastro/login.Criado arquivo routes.js para armazenar as rotas da api. Criado o arquivo auth.js para armazenar a lógica referente a autenticação

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 
-const app = express();
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
-fccTesting(app); //For FCC testing purposes
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -23,14 +24,29 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index', {
-    title: 'Hello',
-    message: 'Please log in'
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: e,
+      message: 'Unable to connect to database'
+    });
   });
 });
 
